93b1547ebed745aae34d78c082e41bbf45b91792
millan-figueroa <millan.fig@gmail.com>
2019-03-22 01:24:52 -0700

    Setup constructor in App.js and TodoList.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React from 'react';
+import TodoList from '../src/components/TodoComponents/TodoList';
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <h2>Millan's ToDo App</h2>
+        <ul>
+          <li>List</li>
+        </ul>
+        <input placeholder="...todo"></input>
+        <button>Add Item</button>
+        <button>Done</button>
       </div>
     );
   }
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,34 @@
-// your components will all go in this `component` directory.
-// feel free to change this component.js into TodoList.js
+import React from 'react';
+
+const TodoArray = [
+   {
+    task: 'Organize Garage',
+    id: 1528817077286,
+    completed: false
+    },
+    {
+    task: 'Bake Cookies',
+    id: 1528817084358,
+    completed: false
+    }
+];
+
+class TodoList extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <div>
+                {TodoArray.map(todo => {
+                    return <li>{todo.task}</li>;
+                })}
+            </div>
+        );
+    }
+}
+
+export default TodoList;
